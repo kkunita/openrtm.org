db37ee9f1bc9497ce221a7e2ffe2356493c1eaa9
Kazuya Kunita <kazuya.kunita@aist.go.jp>
2019-09-18 18:01:28 +0900

    同期作業 2019/9/18時点 CORBA, FAQ完了、OpenRTM-aist デベロッパーズガイド途中

---
+++ Documents/04_DevelopersGuide/04_Advanced_RT_System_Programming/temp.bat
@@ -0,0 +1,64 @@
+cd 09_Local_Service_Use
+copy ..\test.txt 09_Local_Service_Use_ja.txt
+copy ..\test.txt 09_Local_Service_Use_en.txt
+cd ..
+cd 10_SSLTransport_Use
+copy ..\test.txt 10_SSLTransport_Use_ja.txt
+copy ..\test.txt 10_SSLTransport_Use_en.txt
+cd ..
+cd 11_LogicalTimeTriggeredEC_Use
+copy ..\test.txt 11_LogicalTimeTriggeredEC_Use_ja.txt
+copy ..\test.txt 11_LogicalTimeTriggeredEC_Use_en.txt
+cd ..
+cd 12_Create_FSM_Comp
+copy ..\test.txt 12_Create_FSM_Comp_ja.txt
+copy ..\test.txt 12_Create_FSM_Comp_en.txt
+cd ..
+cd 13_CallbackFunction_CallingOrder
+copy ..\test.txt 13_CallbackFunction_CallingOrder_ja.txt
+copy ..\test.txt 13_CallbackFunction_CallingOrder_en.txt
+cd ..
+cd 14_Hierarchy_Composite_Comp_Use
+copy ..\test.txt 14_Hierarchy_Composite_Comp_Use_ja.txt
+copy ..\test.txt 14_Hierarchy_Composite_Comp_Use_en.txt
+cd ..
+cd 15_OpenHRPExecutionContext_Use
+copy ..\test.txt 15_OpenHRPExecutionContext_Use_ja.txt
+copy ..\test.txt 15_OpenHRPExecutionContext_Use_en.txt
+cd ..
+cd 16_ExtTrigExectuionContext_Use
+copy ..\test.txt 16_ExtTrigExectuionContext_Use_ja.txt
+copy ..\test.txt 16_ExtTrigExectuionContext_Use_en.txt
+cd ..
+cd 17_SimulatorExecutionContext_Use
+copy ..\test.txt 17_SimulatorExecutionContext_Use_ja.txt
+copy ..\test.txt 17_SimulatorExecutionContext_Use_en.txt
+cd ..
+cd 18_Implement_Original_Logger
+copy ..\test.txt 18_Implement_Original_Logger_ja.txt
+copy ..\test.txt 18_Implement_Original_Logger_en.txt
+cd ..
+cd 19_CSP_Port_Use
+copy ..\test.txt 19_CSP_Port_Use_ja.txt
+copy ..\test.txt 19_CSP_Port_Use_en.txt
+cd ..
+cd 20_CallbackFunction_Use_by_ComponentAction
+copy ..\test.txt 20_CallbackFunction_Use_by_ComponentAction_ja.txt
+copy ..\test.txt 20_CallbackFunction_Use_by_ComponentAction_en.txt
+cd ..
+cd 21_CallbackFunction_Use_by_ManagerAction
+copy ..\test.txt 21_CallbackFunction_Use_by_ManagerAction_ja.txt
+copy ..\test.txt 21_CallbackFunction_Use_by_ManagerAction_en.txt
+cd ..
+cd 22_Implement_Original_Serializer
+copy ..\test.txt 22_Implement_Original_Serializer_ja.txt
+copy ..\test.txt 22_Implement_Original_Serializer_en.txt
+cd ..
+cd 23_Manager_Functions
+copy ..\test.txt 23_Manager_Functions_ja.txt
+copy ..\test.txt 23_Manager_Functions_en.txt
+cd ..
+cd 24_MasterManager_SlaveManager
+copy ..\test.txt 24_MasterManager_SlaveManager_ja.txt
+copy ..\test.txt 24_MasterManager_SlaveManager_en.txt
+cd ..
